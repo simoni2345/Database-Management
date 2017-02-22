2e475df64f55cce210e8d2e375582d19ac800b64
simoni2345 <simoni.handoo97@gmail.com>
2017-02-21 23:39:29 -0500

    Update Lab4Simoni.sql

--- Lab4Simoni.sql
+++ Lab4Simoni.sql
@@ -91,23 +91,23 @@ AND city not in ('Duluth', 'London');
 /* What are check constraints? 
 -- Check constraints are used to limit the value range that can be placed in a column. CHECK constraints enforce domain integrity by limiting the values that are accepted by one or more columns. You can create a CHECK constraint with any logical (Boolean) expression that returns TRUE or FALSE based on the logical operators. 
 
--- Advantages of Check constraints?
--- By using check constraints, I can ensure that my database only contains data that passes certain constraints. This will allow me to let the database engine control data validation. Having check constraints is a clean way of performing data validation.
+Advantages of Check constraints?
+By using check constraints, I can ensure that my database only contains data that passes certain constraints. This will allow me to let the database engine control data validation. Having check constraints is a clean way of performing data validation.
 
--- Good Example: 
--- Range of values for a salary column. 
--- The range can be limited by creating a CHECK constraint that allows for only Data that ranges from $10,000 through $200,000. This would prevent salaries from being entered beyond the regular salary range. Salary >= 10000 AND salary <= 200000
+Good Example: 
+Range of values for a salary column. 
+The range can be limited by creating a CHECK constraint that allows for only Data that ranges from $10,000 through $200,000. This would prevent salaries from being entered beyond the regular salary range. Salary >= 10000 AND salary <= 200000
 
--- NOT NULL constraint → NOT NULL is exceptionally useful when we have a table with columns that are critical to the definition of that data structure. For instance, an account without a name would be potentially meaningless,
+NOT NULL constraint → NOT NULL is exceptionally useful when we have a table with columns that are critical to the definition of that data structure. For instance, an account without a name would be potentially meaningless,
 
--- You can apply multiple check constraints to a single column.
+You can apply multiple check constraints to a single column.
 
--- Bad Example: 
--- CREATE TABLE IF NOT EXISTS products (
--- Product_id PRIMARY KEY
--- Product_name VARCHAR(255) NOT NULL,
--- Manufacturer TEXT CHECK (Manufacturer > 0)
--- );
--- This would be a bad example of having check constraint as you cannot have a data type of text and then compare it with a numeric value. 
+Bad Example: 
+CREATE TABLE IF NOT EXISTS products (
+Product_id PRIMARY KEY
+Product_name VARCHAR(255) NOT NULL,
+Manufacturer TEXT CHECK (Manufacturer > 0)
+);
+This would be a bad example of having check constraint as you cannot have a data type of text and then compare it with a numeric value. 
 
 */
