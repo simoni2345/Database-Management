12198d0ff3f3ed19876efe95b76a4730623e03b8
simoni2345 <simoni.handoo97@gmail.com>
2017-02-26 12:10:48 -0500

    Update Lab6Simoni.sql

--- Lab6Simoni.sql
+++ Lab6Simoni.sql
@@ -21,15 +21,13 @@ inner join (SELECT avg(priceUSD) as AverageUSD
             FROM Products) 
             as AverageTable
       on AverageUSD < Products.priceUSD
-      ORDER BY Products.name DESC
-; 
+      ORDER BY Products.name DESC; 
 
 -- Query 3
 
 SELECT Customers.name, Orders.pid, Orders.totalUSD
 FROM Customers inner join Orders on Customers.cid = Orders.cid
-ORDER BY Orders.totalUSD ASC
-;
+ORDER BY Orders.totalUSD ASC;
 
 -- Query 4 
 
@@ -41,7 +39,6 @@ inner join (SELECT cid, coalesce(sum(totalUSD) , '0')
             GROUP BY cid
             )as totalTable on totalTable.cid = Customers.cid; 
 
-
 -- Query 5
 
 SELECT Customers.name as "Customer Name" ,  Products.name as "Product Name" , Agents.name as "Agent Name"
@@ -49,24 +46,27 @@ FROM Customers , Agents , Products , Orders
 WHERE Customers.cid = Orders.cid
 AND   Agents.aid = Orders.aid
 AND   Products.pid = Orders.pid
-AND   Agents.city = 'Tokyo'
-;
+AND   Agents.city = 'Tokyo';
 
 -- Query 6
+
 SELECT *
 FROM (SELECT Orders.*, Orders.qty * Products.priceUSD * (1-(discount/100)) AS realdollars
       FROM Orders
       inner join Products on Orders.pid = Products.pid
       inner join Customers on Orders.cid = Customers.cid
       ) AS calcTable
-WHERE totalUSD != realdollars
-;
+WHERE totalUSD != realdollars;
 
 -- Essay Question
 
 
 /*
-Left and Right outer joins:  A left outer join retains all of the rows of the “left” table, regardless of whether there is a row that matches on the “right” table. This means that NULLs are displayed. The “left” table is the table that is written first in the SQL join statement. Right outer join is pretty much the same thing as a left outer join, except that the row from the right table are displayed in the result of the query, regardless of whether or not they have matching values in the left table. 
+Left and Right outer joins:  A left outer join retains all of the rows of the “left” table, regardless of whether there is a 
+row that matches on the “right” table. This means that NULLs are displayed. The “left” table is the table that is written 
+first in the SQL join statement. Right outer join is pretty much the same thing as a left outer join, except that the row 
+from the right table are displayed in the result of the query, regardless of whether or not they have matching values in the 
+left table. 
 
 Example of Left outer join in CAP4 database:
 
